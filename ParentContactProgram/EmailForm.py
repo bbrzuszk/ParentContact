f2f87e16fad75d8fc6ac471c1fc7fdfb45d76cff
Brandon Brzuszkiewicz <bbrzuszk@psd202.org>
2018-05-07 12:07:47 -0500

    Update EmailForm.py

--- ParentContactProgram/EmailForm.py
+++ ParentContactProgram/EmailForm.py
@@ -8,6 +8,19 @@ class ContactGui(object):
         root.geometry("1000x600")
         root.resizable(width=True, height=True)
 
+        #upper frame - Gradebook, message destination
+        lbl_GradeBook = ttk.Label(root, text = "GradeBook: " ,borderwidth = 2, relief = "sunken", )
+        lbl_GradeBook.grid(row = 0, column = 0, columnspan = 2)
+
+        #column Spacer
+        for i in range(10):
+            bt = ttk.Button(root)
+            bt.grid(row = 1, column = i)
+
+
+
+
+
 
 root = Tk()
 Contact = ContactGui(root)
