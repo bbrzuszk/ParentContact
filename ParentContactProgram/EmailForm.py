a5b41ca4406ad37b85700949fac881022372ad15
Brandon Brzuszkiewicz <bbrzuszk@psd202.org>
2018-05-08 15:16:29 -0500

    Update EmailForm.py

--- ParentContactProgram/EmailForm.py
+++ ParentContactProgram/EmailForm.py
@@ -1,48 +1,38 @@
 from tkinter import *
 from tkinter import ttk
+
+
 import sys
 class ContactGui(object):
 
     def __init__(self, root:Tk):
+        print()
         root.title("Parent Contact")
-        root.geometry("1000x600")
+        root.geometry("600x400")
         root.resizable(width=True, height=True)
-        formStyle = ttk.Style()
-        formStyle.configure('TButton', font=('Helvetica', 10))
-        #upper frame - Gradebook, message destination
-        topFrame = Frame(root)
-            #Row 0
-        lbl_Gradebook = ttk.Label(topFrame, text = "Gradebook: ")
-        lbl_Gradebook.grid(row = 0, column = 0, columnspan = 2)
-        entry_Gradebook = Entry(topFrame)
-        entry_Gradebook.grid(row = 0, column = 2, columnspan = 3)
-        btn_GradebookBrowse = ttk.Button(topFrame,text = "Select a Gradebook", style = 'TButton')
-        btn_GradebookBrowse.grid(row = 0, column = 5)
-            # Row 1
-        lbl_MessageDestination = ttk.Label(topFrame, text="Message Destination: ")
-        lbl_MessageDestination.grid(row=1, column=0, columnspan=2)
-        entry_MessageDestination = Entry(topFrame)
-        entry_MessageDestination.grid(row=1, column=2, columnspan=3)
-        btn_MessageBrowse = ttk.Button(topFrame, text="Select a MessageDestination", style='TButton')
-        btn_MessageBrowse.grid(row=1, column=5)
-            # Row 2
-        lbl_Components = ttk.Label(topFrame, text= "Grade Components: ")
-        lbl_Components.grid(row= 2, column = 0, columnspan = 2)
-        btn_Letter = ttk.Button(topFrame, text = "Letter")
-        btn_Letter.grid(row = 2, column = 2)
-        btn_Percentage = ttk.Button(topFrame, text = "Percentage")
-        btn_Percentage.grid(row=2, column=3)
-        btn_MissingWork = ttk.Button(topFrame, text = "Missing Assignments")
-        btn_MissingWork.grid(row = 2, column = 4, columnspan = 2)
-            #Row 3
-        txtArea_Message = Text(topFrame, width = 40, height = 20)
-        txtArea_Message.grid(row = 3, column = 0, columnspan = 4, rowspan = 6, pady = 20)
-        lbl_pronounButts = ttk.Label(topFrame, text = "Pronoun's")
-        lbl_pronounButts.grid(row = 3, column = 4, columnspan = 2)
+        ttk.Style().theme_use('vista')
+        self.gradebookString = StringVar()
+        self.buildTopInputs(root)
 
 
 
-        topFrame.grid()
+    def buildTopInputs(self, master):
+        frame = LabelFrame(master, text = "Inputs")
+
+        frame.grid()
+        ttk.Label(frame, text = "Gradebook: ").grid(row = 0, column = 0, sticky = "w")
+        self.gradebookEntry = ttk.Entry(frame, cursor = "xterm" , width = "60", textvariable = self.gradebookString )
+        self.gradebookEntry.grid(row = 0, column = 1, sticky = "e")
+        self.gradebookBrowse = ttk.Button(frame, text = "...", command = self.getGradeBook)
+        self.gradebookBrowse.grid(row = 0, column = 2)
+
+        ttk.Label(frame, text="Message Destination: ").grid(row=1, column=0, sticky = "w")
+
+
+    def getGradeBook(self):
+        print("getting Gradebook")
+        self.gradebookString.set("getting Gradebook")
+
 
 
 
@@ -52,6 +42,7 @@ class ContactGui(object):
 
 
 root = Tk()
+
 Contact = ContactGui(root)
 root.mainloop()
 sys.exit("closed")
