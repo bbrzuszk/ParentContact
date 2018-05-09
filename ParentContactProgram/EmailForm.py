2ec9b36e89033d56a32392c0d9d6909cc42f07d5
Brandon Brzuszkiewicz <bbrzuszk@psd202.org>
2018-05-09 11:04:21 -0500

    Update EmailForm.py

--- ParentContactProgram/EmailForm.py
+++ ParentContactProgram/EmailForm.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 
 import sys
 class ContactGui(object):
-
+    names = ('steve', 'jen', 'bill', 'carter')
     def __init__(self, root:Tk):
         print()
         root.title("Parent Contact")
@@ -12,14 +12,16 @@ class ContactGui(object):
         root.resizable(width=True, height=True)
         ttk.Style().theme_use('vista')
         self.gradebookString = StringVar()
+        self.messageDestinationString = StringVar()
         self.buildTopInputs(root)
 
 
 
     def buildTopInputs(self, master):
+        """Builds top frame for message input components"""
         frame = LabelFrame(master, text = "Inputs")
 
-        frame.grid()
+        frame.grid(padx = 20)
         ttk.Label(frame, text = "Gradebook: ").grid(row = 0, column = 0, sticky = "w")
         self.gradebookEntry = ttk.Entry(frame, cursor = "xterm" , width = "60", textvariable = self.gradebookString )
         self.gradebookEntry.grid(row = 0, column = 1, sticky = "e")
@@ -27,14 +29,33 @@ class ContactGui(object):
         self.gradebookBrowse.grid(row = 0, column = 2)
 
         ttk.Label(frame, text="Message Destination: ").grid(row=1, column=0, sticky = "w")
+        self.messageEntry = ttk.Entry(frame, cursor = "xterm" , width = "60", textvariable = self.messageDestinationString)
+        self.messageEntry.grid(row=1, column=1, sticky="e")
+        self.messageBrowse = ttk.Button(frame, text="...", command = self.getMessageDestinationFolder)
+        self.messageBrowse.grid(row=1, column=2)
+
+        ttk.Label(frame, text = "Select Student(s): ").grid(row = 2, column = 0, sticky = "w")
+
+        self.studentDropDownMenu = Listbox(frame, selectmode = MULTIPLE, height = 1, activestyle = "none")
+       # yScroll = Scrollbar(self.studentDropDownMenu,orient=VERTICAL)
+       # yScroll.grid(row=0, column=1, sticky="ns")
+      #  yScroll['command'] = self.studentDropDownMenu.yview()
+        self.buildStudentMenu()
+        self.studentDropDownMenu.grid(row = 2, column = 1, sticky = "w")
+
+
 
 
     def getGradeBook(self):
         print("getting Gradebook")
         self.gradebookString.set("getting Gradebook")
 
+    def getMessageDestinationFolder(self):
+        self.messageDestinationString.set("getting message destination")
 
-
+    def buildStudentMenu(self):
+        for i in range(len(ContactGui.names)):
+            self.studentDropDownMenu.insert(i, ContactGui.names[i])
 
 
 
